ab4f6a8315d2f2e99a6bff1ee41c5db92ed4f770
Adam Strickland <adam.strickland@gmail.com>
2010-11-19 17:08:50 -0600

    seed

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,4 +1,5 @@
 class Division < ActiveRecord::Base
   belongs_to :group
   has_many :coverages
+  has_many :participants
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,56 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-require 'spec/support/blueprints'
+require File.join(Rails.root, 'spec', 'support', 'blueprints')
 
-10.times{ Participant.make }
+# 10.times{ 
+#   Participant.make
+# }
+
+locs = %w(Medical Dental Vision)
+
+groups = (0..rand(10)).map{ 
+  group = Group.make
+  divisions = (0..rand(3)).map{ 
+    division = Division.make(:group => group) 
+    (0..rand(100)).map{ Participant.make(:division => division) }
+    division
+  }
+  group
+}
+
+carriers = (0..4).map{
+  carrier = Carrier.make
+  plans = locs.map{|loc| (0..rand(5)).map{ Plan.make(:line_of_coverage => loc, :carrier => carrier) }}.flatten
+  carrier
+}
+
+brokers = (1..10).map{ Broker.make }
+
+coverages = groups.map{ |group|
+  group.divisions.map{ |div|
+    carriers.rand.plans.map{ |plan|
+      Coverage.make({
+        :division => div,
+        :brokers => [brokers.rand],
+        :plan => plan
+      })
+    }
+  }.flatten
+}.flatten
+
+participants = groups.map{ |g| g.divisions.map{ |d| d.participants }.flatten }.flatten
+
+policies = participants.map{ |participant|
+  participant.division.coverages.map{ |coverage|
+    Policy.make({
+      :coverage => coverage,
+      :participant => participant
+    })
+  }
+}
+
+puts "Results:"
+%w(Group Division Carrier Plan Participant Policy Coverage Broker).each do |w|
+  puts "  #{w.constantize.send(:count)} #{w.pluralize}"
+end
