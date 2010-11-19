1e35b4bea6ded2872899033145c13fdd016c38fb
Adam Strickland <adam.strickland@gmail.com>
2010-11-18 18:09:34 -0600

    wrong rails ver

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodenameSpidermonkey::Application.config.secret_token = 'd2b18ea82555d72a42b045496c5b10253ad9067e630b10b31b06c526442954ca3e9a0dca58a75fb040ae6a6e180eb5a9182bd26fdc76d9856b01477cf2c0cd87'
